94d209c1a96046651d2b2de6e6fac677fb746935
Nguyen Pham <nguyenpham0806@gmail.com>
2026-08-03 08:25:05 -0700

    Initial Commit

---
+++ Watch Minigames Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Watch Minigames Watch App
+//
+//  Created by Nguyen Pham on 8/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Watch Minigames Watch App/Watch_MinigamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_MinigamesApp.swift
+//  Watch Minigames Watch App
+//
+//  Created by Nguyen Pham on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Minigames_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
